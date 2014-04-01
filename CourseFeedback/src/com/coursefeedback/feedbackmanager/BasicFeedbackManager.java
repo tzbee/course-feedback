4c1e14b7314b3b5fecc9372129a367b0b55961c4
touzbi <tolbimine@gmail.com>
2014-04-01 18:39:34 +0300

    Added:   - get all feedbacks from database -> get-all-feedbacks.xhtml

--- CourseFeedback/src/com/coursefeedback/feedbackmanager/BasicFeedbackManager.java
+++ CourseFeedback/src/com/coursefeedback/feedbackmanager/BasicFeedbackManager.java
@@ -1,22 +1,31 @@
 package com.coursefeedback.feedbackmanager;
 
+import java.util.Collection;
+
 import javax.faces.bean.ManagedBean;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import com.coursefeedback.feedback.Feedback;
 
 @ManagedBean
 public class BasicFeedbackManager implements FeedbackManager {
+	private EntityManager em = Persistence.createEntityManagerFactory(
+			"CourseFeedback").createEntityManager();
+
 	@Override
 	public String saveFeedback(Feedback feedback) {
-		EntityManager em = Persistence.createEntityManagerFactory(
-				"CourseFeedback").createEntityManager();
-
-		em.getTransaction().begin();
-		em.persist(feedback);
-		em.getTransaction().commit();
+		this.em.getTransaction().begin();
+		this.em.persist(feedback);
+		this.em.getTransaction().commit();
 
 		return "feedbackBar";
+	}
+
+	@Override
+	public Collection<Feedback> getAllFeedbacks() {
+		Query query = this.em.createQuery("SELECT f FROM Feedback f");
+		return (Collection<Feedback>) query.getResultList();
 	}
 }
--- CourseFeedback/src/com/coursefeedback/feedbackmanager/FeedbackManager.java
+++ CourseFeedback/src/com/coursefeedback/feedbackmanager/FeedbackManager.java
@@ -1,7 +1,11 @@
 package com.coursefeedback.feedbackmanager;
 
+import java.util.Collection;
+
 import com.coursefeedback.feedback.Feedback;
 
 public interface FeedbackManager {
 	String saveFeedback(Feedback feedback);
+	
+	Collection<Feedback> getAllFeedbacks();
 }
