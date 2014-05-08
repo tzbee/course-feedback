3bc2f85f975b4ed7a2556029a0192febcecc0d86
touzbi <tolbimine@gmail.com>
2014-05-08 17:34:29 +0300

    Refactoring   comments in feedback manager Added Javadoc(API)

--- CourseFeedback/src/com/coursefeedback/feedbackmanager/FeedbackManager.java
+++ CourseFeedback/src/com/coursefeedback/feedbackmanager/FeedbackManager.java
@@ -4,11 +4,32 @@ import java.util.Collection;
 
 import com.coursefeedback.feedback.Feedback;
 
+/**
+ * Feedback Manager class
+ * Handles all feedback operations
+ * @author Amine
+ *
+ */
 public interface FeedbackManager {
+	/**
+	 * 
+	 * @return all registered feedbacks
+	 */
 	Collection<Feedback> getAllFeedbacks();
 
+	/**
+	 * Adds feedback to a course item
+	 * @param feedback
+	 * @param courseId
+	 * @return
+	 */
 	String addFeedbackToCourseItem(Feedback feedback, int courseId);
 
+	/**
+	 * Adds feedback to the system
+	 * @param feedback
+	 * @return
+	 */
 	String addFeedback(Feedback feedback);
 
 	/**
