f0e5ff615fab763edcba8d1df683a99c1a852f61
touzbi <tolbimine@gmail.com>
2014-05-12 23:35:55 +0300

    Refactoring New test source folder

---
+++ CourseFeedback/src_test/com/coursefeedbacktest/course/CourseBeanTest.java
@@ -0,0 +1,44 @@
+package com.coursefeedbacktest.course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+import com.coursefeedback.course.Course;
+
+@ManagedBean(name = "courseBeanTest")
+public class CourseBeanTest {
+	private List<Course> courses = new ArrayList<Course>();
+
+	// Test values
+	private static final String[] COURSE_CODES = { "H454", "S65987", "FEF",
+			"FE656" };
+	private static final String[] COURSE_NAMES = { "IT", "Physics", "Maths",
+			"Geography" };
+
+	@PostConstruct
+	public void initCourses() {
+		Course course;
+
+		try {
+			for (int i = 0; i < COURSE_CODES.length; i++) {
+				course = new Course();
+				course.setCourseCode(COURSE_CODES[i]);
+				course.setName(COURSE_NAMES[i]);
+				this.courses.add(course);
+			}
+		} catch (IndexOutOfBoundsException ignore) {
+			// Do nothing
+		}
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+}
